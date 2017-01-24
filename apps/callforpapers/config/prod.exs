a547629ac96d1a99053661ff241b7e5eea31b629
David Schmitz <david.schmitz.privat@gmail.com>
2017-01-24 11:04:54 +0100

    Prepare for heroku

--- apps/callforpapers/config/prod.exs
+++ apps/callforpapers/config/prod.exs
@@ -13,7 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :callforpapers, Callforpapers.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https",
+        host: "callforpapers.herokuapp.com", port: 443],
+        force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
@@ -58,4 +60,15 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+#
+
+config :callforpapers, Callforpapers.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :callforpapers, Callforpapers.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
