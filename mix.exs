5a05cb08e6ddba7b66c4af76a4f68e4ce7f2c900
David Schmitz <david.schmitz.privat@gmail.com>
2017-01-25 20:45:06 +0100

    Fixed mix typo

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Callforpapers.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:travis), do elixirc_paths(:test)
+  defp elixirc_paths(:travis), do: elixirc_paths(:test)
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
