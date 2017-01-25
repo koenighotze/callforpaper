4043957982576657e1f13ac6363500a0761bfc07
David Schmitz <david.schmitz.privat@gmail.com>
2017-01-25 19:57:52 +0100

    Fixed module names and db setup

--- config/travis.exs
+++ config/travis.exs
@@ -2,15 +2,15 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :callforpapers, CfpUi.Endpoint,
-  http: [port: 4011],
+config :callforpapers, Callforpapers.Endpoint,
+  http: [port: 4000],
   server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warn
 
 # Configure your database
-config :callforpapers, CfpUi.Repo,
+config :callforpapers, Callforpapers.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "",
