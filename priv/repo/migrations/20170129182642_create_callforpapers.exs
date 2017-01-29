a15faf54f1fd06fd3b79d63ca188ab84b32497a9
David Schmitz <david.schmitz.privat@gmail.com>
2017-01-29 20:59:11 +0100

    fixed tests and migration

--- priv/repo/migrations/20170129182642_create_callforpapers.exs
+++ priv/repo/migrations/20170129182642_create_callforpapers.exs
@@ -2,6 +2,8 @@ defmodule Callforpapers.Repo.Migrations.CreateCallforpapers do
   use Ecto.Migration
 
   def change do
+    drop_if_exists table(:callforpapers)
+
     create table(:callforpapers) do
       add :start, :date
       add :end, :date
--- test/controllers/callforpapers_controller_test.exs
+++ test/controllers/callforpapers_controller_test.exs
@@ -5,45 +5,61 @@ defmodule Callforpapers.CallforpapersControllerTest do
   @valid_attrs %{end: %{day: 17, month: 4, year: 2010}, start: %{day: 17, month: 4, year: 2010}, status: "some content"}
   @invalid_attrs %{}
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, callforpapers_path(conn, :index)
     assert html_response(conn, 200) =~ "Listing callforpapers"
   end
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "renders form for new resources", %{conn: conn} do
     conn = get conn, callforpapers_path(conn, :new)
     assert html_response(conn, 200) =~ "New callforpapers"
   end
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "creates resource and redirects when data is valid", %{conn: conn} do
     conn = post conn, callforpapers_path(conn, :create), callforpapers: @valid_attrs
     assert redirected_to(conn) == callforpapers_path(conn, :index)
     assert Repo.get_by(Callforpapers, @valid_attrs)
   end
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
     conn = post conn, callforpapers_path(conn, :create), callforpapers: @invalid_attrs
     assert html_response(conn, 200) =~ "New callforpapers"
   end
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "shows chosen resource", %{conn: conn} do
     callforpapers = Repo.insert! %Callforpapers{}
     conn = get conn, callforpapers_path(conn, :show, callforpapers)
     assert html_response(conn, 200) =~ "Show callforpapers"
   end
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "renders page not found when id is nonexistent", %{conn: conn} do
     assert_error_sent 404, fn ->
       get conn, callforpapers_path(conn, :show, -1)
     end
   end
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "renders form for editing chosen resource", %{conn: conn} do
     callforpapers = Repo.insert! %Callforpapers{}
     conn = get conn, callforpapers_path(conn, :edit, callforpapers)
     assert html_response(conn, 200) =~ "Edit callforpapers"
   end
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "updates chosen resource and redirects when data is valid", %{conn: conn} do
     callforpapers = Repo.insert! %Callforpapers{}
     conn = put conn, callforpapers_path(conn, :update, callforpapers), callforpapers: @valid_attrs
@@ -51,12 +67,16 @@ defmodule Callforpapers.CallforpapersControllerTest do
     assert Repo.get_by(Callforpapers, @valid_attrs)
   end
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
     callforpapers = Repo.insert! %Callforpapers{}
     conn = put conn, callforpapers_path(conn, :update, callforpapers), callforpapers: @invalid_attrs
     assert html_response(conn, 200) =~ "Edit callforpapers"
   end
 
+  @tag login_as: "max"
+  @tag :as_organizer
   test "deletes chosen resource", %{conn: conn} do
     callforpapers = Repo.insert! %Callforpapers{}
     conn = delete conn, callforpapers_path(conn, :delete, callforpapers)
